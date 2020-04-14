406504c90c0fc80cd72a2e371af221ace72d69d4
dkv-test <michael.klose@dkv-mobility.com>
2020-04-14 10:18:09 +0000

    init

---
+++ exercise/readme.md
@@ -0,0 +1,1 @@
+run tests: mvn test -Dtest=ExerciseTest
--- exercise/src/test/java/com/dkv/ExerciseTest.java
+++ exercise/src/test/java/com/dkv/ExerciseTest.java
@@ -1,3 +1,5 @@
+package com.dkv;
+
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -8,7 +10,7 @@ public class ExerciseTest {
     @Test
     public void testExercise() {
         int i = 1;
-        assertThat(i, equalTo(new Integer(1)));
+        assertThat(i, equalTo(1));
         System.out.println("done");
     }
 }
