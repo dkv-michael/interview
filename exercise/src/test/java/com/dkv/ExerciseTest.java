a3fd8de1cea6e6ce5da4aa7428ea74c142f72338
dkv-test <michael.klose@dkv-mobility.com>
2020-04-21 08:02:53 +0000

    add test

--- exercise/src/test/java/com/dkv/ExerciseTest.java
+++ exercise/src/test/java/com/dkv/ExerciseTest.java
@@ -3,14 +3,30 @@ package com.dkv;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.List;
+import java.util.Set;
 
 public class ExerciseTest {
 
+    private Set<String> extractFromList(List<String> source, String ... toExtract) {
+        // TODO: implement this
+        return null;
+    }
+
     @Test
     public void testExercise() {
-        int i = 1;
-        assertThat(i, equalTo(1));
-        System.out.println("done");
+        // given
+        String toExtract = "a";
+        List<String> src = List.of("a", "b", "c");
+
+        // when
+        Set<String> result = extractFromList(src, toExtract);
+
+        // then
+        assertThat(result.size(), equalTo(1));
+        assertThat(result, hasItem(toExtract));
     }
 }
